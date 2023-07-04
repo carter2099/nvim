fefdb68cd6efc6b742c6fce698906eb3c1e30442
big-charter <bigcharter@protonmail.com>
2023-07-03 22:01:36 -0400

    Add lua lsp. Fix spacing

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -3,33 +3,37 @@ local lsp = require('lsp-zero')
 lsp.preset('recommended')
 
 lsp.ensure_installed({
-	'clangd',
-	'cssls',
-	'dockerls',
-	'docker_compose_language_service',
-	'eslint',
-	'gradle_ls',
-	'html',
+    'clangd',
+    'cssls',
+    'dockerls',
+    'docker_compose_language_service',
+    'eslint',
+    'gradle_ls',
+    'html',
     'jdtls',
-	'jsonls',
-	'tsserver',
-	'marksman',
-	'pyre',
-	'sqlls',
-	'solidity',
-})	
+    'jsonls',
+    'tsserver',
+    'lua_ls',
+    'marksman',
+    'pyre',
+    'sqlls',
+    'solidity',
+})
+
+-- Fix Undefined global 'vim'
+lsp.nvim_workspace()
 
 local cmp = require('cmp')
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
 local cmp_mappings = lsp.defaults.cmp_mappings({
-	['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-	['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-	['<C-y>'] = cmp.mapping.confirm({ select = true }),
-	["<C-Space>"] = cmp.mapping.complete(),
+    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+    ["<C-Space>"] = cmp.mapping.complete(),
 })
 
 lsp.setup_nvim_cmp({
-  mapping = cmp_mappings
+    mapping = cmp_mappings
 })
 
 lsp.setup()
