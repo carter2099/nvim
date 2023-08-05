481ff2758f91cdedcbcf759b8aabb37dd86f6300
big-charter <bigcharter@protonmail.com>
2023-08-05 18:05:38 -0400

    remap to use clipboard register

--- lua/bigcharter/remap.lua
+++ lua/bigcharter/remap.lua
@@ -10,13 +10,6 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
-vim.keymap.set("n", "<leader>vwm", function()
-    require("vim-with-me").StartVimWithMe()
-end)
-vim.keymap.set("n", "<leader>svwm", function()
-    require("vim-with-me").StopVimWithMe()
-end)
-
 -- greatest remap ever
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
@@ -43,6 +36,10 @@ vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/theprimeagen/packer.lua<CR>")
 vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>")
+
+vim.keymap.set("v", "<leader>y", '"+y')
+vim.keymap.set("v", "<leader>d", '"+d')
+vim.keymap.set("n", "<leader>p", '"+p')
 
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
