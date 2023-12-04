3e78ae7b13ae443cbf7fd433419158c19d784dea
big-charter <bigcharter@protonmail.com>
2023-12-03 20:13:28 -0500

    Minimal - start to strip

--- lua/bigcharter/packer.lua
+++ lua/bigcharter/packer.lua
@@ -25,8 +25,6 @@ return require("packer").startup(function(use)
     use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
     use("nvim-treesitter/playground")
     use("theprimeagen/harpoon")
-    use("mbbill/undotree")
-    use("tpope/vim-fugitive")
     use("jwalton512/vim-blade")
 
     use({
--- lua/bigcharter/set.lua
+++ lua/bigcharter/set.lua
@@ -7,6 +7,7 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
 vim.opt.smartindent = true
+vim.opt.autoindent = true
 
 vim.opt.wrap = false
 
@@ -22,8 +23,5 @@ vim.opt.termguicolors = true
 
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
-vim.opt.isfname:append("@-@")
-
-vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = "80"
