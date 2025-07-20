110914ebabff149081ec8054687428902a34fe68
carter2099 <carter2077@pm.me>
2025-07-20 13:24:19 -0400

    reorg color schemes

---
+++ lua/plugins/colorschemes/vscode.lua
@@ -0,0 +1,10 @@
+return {
+    "Mofiqul/vscode.nvim",
+    name = "vscode",
+    config = function()
+        require("vscode").setup({
+            transparent = true
+        })
+        vim.cmd("colorscheme vscode")
+    end
+}
